d613c132d517eeb5ee2936f1562acabccd586b5e
William-Hill <mjh2225@gmail.com>
2018-02-26 17:29:15 -0800

    uncommented syncdb

--- cog/installation/install.py
+++ cog/installation/install.py
@@ -63,7 +63,7 @@ class CoGInstall(object):
             raise Exception("Unknow database type: %s" % dbtype)
         
         # django management commands
-        #management.call_command("syncdb", interactive=False)
+        management.call_command("syncdb", interactive=False)
         #management.call_command("migrate","--fake-initial")
         management.call_command("migrate", interactive=False)
         management.call_command("collectstatic", interactive=False, verbosity=0)
